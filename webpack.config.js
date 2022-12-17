f1e78d775b7707b9bbb030ac0956bed611fe02b9
Roman Medvedtckiy <roman.medvedtckiy@gmail.com>
2022-12-18 00:59:32 +0300

    Настроил сборку webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  entry: './src/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.[contenthash].js"
+  },
+  module: {
+    rules: [{
+      // это правило указывает на то, что бы webpack прогнал все файлы с расширением js через babel
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+      //css-loader обрабатывает файлы стилей и кладёт результат в js в виде строк
+      //style-loader переносит полученые стили в тег style
+      test: /\.css$/,
+      use: ['style-loader','css-loader']
+    },{
+      test: /\.svg$/,
+      type: "asset/resource"
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })],
+  devServer: {
+    open: true
+  }
+}
